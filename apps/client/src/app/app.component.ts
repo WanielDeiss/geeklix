3d5865be013ff10b6156eb7e00cd2c66c5900bf2
Daniel Weiß <wanieldeiss@gmail.com>
2021-01-16 08:55:05 +0100

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@geeklix/api-interfaces';
+
+@Component({
+  selector: 'geeklix-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
