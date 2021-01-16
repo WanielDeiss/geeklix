d204b3ff7264544e0737ccc8e082ea5f3aa1867b
Daniel Weiß <wanieldeiss@gmail.com>
2021-01-16 09:14:33 +0100

    remove boilercode from client

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -7,7 +7,4 @@ import { Message } from '@geeklix/api-interfaces';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
-}
+export class AppComponent {}
